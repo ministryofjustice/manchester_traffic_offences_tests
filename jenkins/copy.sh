29cd093b160707d7522efc8243d5dfebf87f4c13
Ian George <ian.george@digital.justice.gov.uk>
2015-07-07 16:13:09 +0100

    Added scripts to recreate jenkins environment on AWS

---
+++ jenkins/copy.sh
@@ -0,0 +1,3 @@
+scp -r -i ../../manchester_traffic_offences_ops/pillars/manchester.pem * ubuntu@jenkins.makeaplea.dsd.io:~
+scp -r -i ../../manchester_traffic_offences_ops/pillars/manchester.pem .ssh ubuntu@jenkins.makeaplea.dsd.io:~
+ssh -i ../../manchester_traffic_offences_ops/pillars/manchester.pem ubuntu@jenkins.makeaplea.dsd.io
